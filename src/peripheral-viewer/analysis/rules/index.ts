f3a63c16dfb9c9e56a862baaf0e2c79f4c9b94f3
HalfSweet <HalfSweet@HalfSweet.cn>
2026-03-30 23:48:11 +0800

    feat(debug): add gpio peripheral analyzer rules

--- src/peripheral-viewer/analysis/rules/index.ts
+++ src/peripheral-viewer/analysis/rules/index.ts
@@ -1,8 +1,10 @@
 import { PeripheralAnalysisRuntime } from '../runtime';
 import { registerSf32lb52xAnalyzers } from './sf32lb52x';
+import { registerSf32lb52xGpioAnalyzers } from './sf32lb52x_gpio';
 import { registerSf32lb56xAnalyzers } from './sf32lb56x';
 
 export function registerBuiltInPeripheralAnalyzers(runtime: PeripheralAnalysisRuntime): void {
   registerSf32lb52xAnalyzers(runtime);
+  registerSf32lb52xGpioAnalyzers(runtime);
   registerSf32lb56xAnalyzers(runtime);
 }
